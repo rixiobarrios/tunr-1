cd3e62158b4e554314dc34219bf7fcc57dc5a3d8
Ali Spittel <ali07cat07@gmail.com>
2018-04-11 12:28:02 -0400

    adding solution for models class

--- tunr/templatetags/favorite_tags.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name='has_favorite')
-def has_favorite(song, user):
-    return song.favorites.filter(user=user).exists()
